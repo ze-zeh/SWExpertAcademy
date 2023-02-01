ea756245ba27e985532480359dac398a095fe93d
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-02-01 14:14:00 +0900

    Create 12304. 기초 Double Linked List 연습-Main.java

---
+++ 12304. 기초 Double Linked List 연습-Main.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	private final static int MAX_NODE = 10000;
+	private final static int ADD_HEAD = 1;
+	private final static int ADD_TAIL = 2;
+	private final static int ADD_NUM = 3;
+	private final static int FIND = 4;
+	private final static int REMOVE = 5;
+	private final static int PRINT = 6;
+	private final static int PRINT_REVERSE = 7;
+	private final static int END = 99;
+	
+	private final static UserSolution usersolution = new UserSolution();
+	
+	private static BufferedReader br;
+	
+	public static void run() throws Exception {
+		int cmd, data, num, ret;
+		int[] output = new int[MAX_NODE];
+		String str;
+		StringTokenizer st;
+		
+		while(true) {
+			str = br.readLine();
+			st = new StringTokenizer(str, " ");
+			cmd = Integer.parseInt(st.nextToken());
+			
+			switch(cmd) {
+			case ADD_HEAD :
+				data = Integer.parseInt(st.nextToken());
+				usersolution.addNode2Head(data);
+				break;
+			
+			case ADD_TAIL :
+				data = Integer.parseInt(st.nextToken());
+				usersolution.addNode2Tail(data);
+				break;
+			
+			case ADD_NUM :
+				data = Integer.parseInt(st.nextToken());
+				num = Integer.parseInt(st.nextToken());
+				usersolution.addNode2Num(data, num);
+				break;
+				
+			case FIND :
+				data = Integer.parseInt(st.nextToken());
+				num = usersolution.findNode(data);
+				System.out.println(num);
+				break;
+				
+			case REMOVE :
+				data = Integer.parseInt(st.nextToken());
+				usersolution.removeNode(data);
+				break;
+			
+			case PRINT :
+				ret = usersolution.getList(output);
+				for(int i = 0; i < ret; i++) {
+					System.out.print(output[i] + " ");
+				}
+				System.out.println();
+				break;
+			
+			case PRINT_REVERSE :
+				ret = usersolution.getReversedList(output);
+				for(int i = 0; i < ret; i++) {
+					System.out.print(output[i] + " ");
+				}
+				System.out.println();
+				break;
+				
+			case END :
+				return;
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		int TC;
+		//System.setIn(new java.io.FileInputStream("res/sample_input.txt"));
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		TC = Integer.parseInt(str);
+		
+		for (int tc = 1; tc <= TC; tc++) {
+			System.out.println("#" + tc);
+			usersolution.init();
+			run();
+			System.out.println();
+		}
+	}
+}
