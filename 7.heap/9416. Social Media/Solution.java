401df0f48cffb8b00b3edc4e29504d3ed65d1455
JaeHun Jo <58066704+ze-zeh@users.noreply.github.com>
2023-02-23 17:17:37 +0900

    Update Solution.java

--- 7.heap/9416. Social Media/Solution.java
+++ 7.heap/9416. Social Media/Solution.java
@@ -1,66 +1,168 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-class UserSolution {
-    static Map<Integer, Post> posts;
-    static boolean[][] following;
+class Solution {
 
-    static class Post {
-        int pID;
-        int uID;
-        int timestamp;
-        int like;
+	private static int mSeed;
+	private static int pseudo_rand()
+	{
+		mSeed = mSeed * 431345 + 2531999;
+		return mSeed & 0x7FFFFFFF;
+	}
 
-        Post(int pID, int uID, int timestamp) {
-            this.pID = pID;
-            this.uID = uID;
-            this.timestamp = timestamp;
-            this.like = 0;
-        }
-    }
+	private static int follow_status[][] = new int[1005][1005];
+	private static int answer_score;
+	private static int n; // n >= 5 && n <= 1000
+	private static int end_timestamp;
+	private static int follow_ratio; // follow_ratio >= 1 && follow_ratio <= 10000
+	private static int make_ratio; // make_ratio >= 1 && make_ratio <= 10000
+	private static int like_ratio; // like_ratio >= 1 && like_ratio <= 10000
+	private static int get_feed_ratio; // get_feed_ratio >= 1 && get_feed_ratio <= 10000
+	private static int post_arr[] = new int[200000];
+	private static int total_post_cnt;
+	private static int min_post_cnt;
+	
+	private static BufferedReader br;
+	private static UserSolution user = new UserSolution();
 
-    public void init(int N) {
-        posts = new HashMap<>();
-        following = new boolean[N + 1][N + 1];
+	private static boolean run() throws Exception
+	{
+		int uId1;
+		int uId2;
+		int pId;
+		int pIdList[] = new int[10];
+		int ans_pIdList[] = new int[10];
+		int rand_val;
+		boolean ret = true;
 
-        for (int i = 1; i <= N; i++) following[i][i] = true;
-    }
+		StringTokenizer stdin = new StringTokenizer(br.readLine(), " ");
+		mSeed = Integer.parseInt(stdin.nextToken());
+		n = Integer.parseInt(stdin.nextToken());
+		end_timestamp = Integer.parseInt(stdin.nextToken());
+		follow_ratio = Integer.parseInt(stdin.nextToken());
+		make_ratio = Integer.parseInt(stdin.nextToken());
+		like_ratio = Integer.parseInt(stdin.nextToken());
+		get_feed_ratio = Integer.parseInt(stdin.nextToken());
+		
+		user.init(n);
 
-    public void follow(int uID1, int uID2, int timestamp) {
-        following[uID1][uID2] = true;
-    }
+		for (uId1 = 1; uId1 <= n; uId1++)
+		{
+			follow_status[uId1][uId1] = 1;
+			int m = n / 10 + 1;
+			if (m > 10)
+				m = 10;
+			for (int i = 0; i < m; i++)
+			{
+				uId2 = uId1;
+				while (follow_status[uId1][uId2] == 1)
+				{
+					uId2 = pseudo_rand() % n + 1;
+				}
+				user.follow(uId1, uId2, 1);
+				follow_status[uId1][uId2] = 1;
+			}
+		}
+		min_post_cnt = total_post_cnt = 1;
 
-    public void makePost(int uID, int pID, int timestamp) {
-        posts.put(pID, new Post(pID, uID, timestamp));
-    }
+		for (int timestamp = 1; timestamp <= end_timestamp; timestamp++)
+		{
+			rand_val = pseudo_rand() % 10000;
+			if (rand_val < follow_ratio)
+			{
+				uId1 = pseudo_rand() % n + 1;
+				uId2 = pseudo_rand() % n + 1;
+				int lim = 0;
+				while (follow_status[uId1][uId2] == 1 || uId1 == uId2)
+				{
+					uId2 = pseudo_rand() % n + 1;
+					lim++;
+					if (lim >= 5)
+						break;
+				}
+				if (follow_status[uId1][uId2] == 0)
+				{
+					user.follow(uId1, uId2, timestamp);
+					follow_status[uId1][uId2] = 1;
+				}
+			}
+			rand_val = pseudo_rand() % 10000;
 
-    public void like(int pID, int timestamp) {
-        posts.get(pID).like++;
-    }
+			if (rand_val < make_ratio)
+			{
+				uId1 = pseudo_rand() % n + 1;
+				post_arr[total_post_cnt] = timestamp;
 
-    public void getFeed(int uID, int timestamp, int pIDList[]) {
-        PriorityQueue<Post> pq1 = new PriorityQueue<>((o1, o2) -> {
-            if (o1.like == o2.like) return o2.timestamp - o1.timestamp;
-            return o2.like - o1.like;
-        });
+				user.makePost(uId1, total_post_cnt, timestamp);
+				total_post_cnt += 1;
+			}
 
-        PriorityQueue<Post> pq2 = new PriorityQueue<>((o1, o2) -> o2.timestamp - o1.timestamp);
+			rand_val = pseudo_rand() % 10000;
 
-        for (int i = posts.size(); i >= 1; i--) {
-            Post post = posts.get(i);
-            if (following[uID][post.uID]) {
-                if (timestamp - 1000 <= post.timestamp) {
-                    pq1.add(post);
-                } else {
-                    pq2.add(post);
-                    if (pq1.size() + pq2.size() >= 10) break;
-                }
-            }
-        }
+			if (rand_val < like_ratio && total_post_cnt - min_post_cnt > 0)
+			{
+				while (post_arr[min_post_cnt] < timestamp - 1000 && min_post_cnt < total_post_cnt)
+					min_post_cnt++;
 
-        int listSize = 0;
-        while (!pq1.isEmpty() && listSize < 10) pIDList[listSize++] = pq1.poll().pID;
-        while (!pq2.isEmpty() && listSize < 10) pIDList[listSize++] = pq2.poll().pID;
-    }
+				if (total_post_cnt != min_post_cnt)
+				{
+					pId = pseudo_rand() % (total_post_cnt - min_post_cnt) + min_post_cnt;
+					user.like(pId, timestamp);
+				}
+			}
+
+			rand_val = pseudo_rand() % 10000;
+			if (rand_val < get_feed_ratio && total_post_cnt > 0)
+			{
+				uId1 = pseudo_rand() % n + 1;
+				user.getFeed(uId1, timestamp, pIdList);
+				stdin = new StringTokenizer(br.readLine(), " ");
+
+				for (int i = 0; i < 10; i++)
+				{
+					ans_pIdList[i] = Integer.parseInt(stdin.nextToken()); 
+				}
+
+				for (int i = 0; i < 10; i++)
+				{
+					if (ans_pIdList[i] == 0)
+						break;
+
+					if (ans_pIdList[i] != pIdList[i])
+					{
+						ret = false;
+					}
+				}
+			}
+		}
+
+		return ret;
+	}
+	
+
+	public static void main(String[] args) throws Exception {
+//		System.setIn(new java.io.FileInputStream("/Users/hun/IdeaProjects/algorithm-java/src/sample_sample_input.txt"));
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int tc;
+		StringTokenizer stdin = new StringTokenizer(br.readLine(), " ");
+		tc = Integer.parseInt(stdin.nextToken());
+		answer_score = Integer.parseInt(stdin.nextToken());
+
+		for (int t = 1; t <= tc; t++)
+		{
+			int score;
+			for (int i = 0; i < 1005; i++)
+				for (int j = 0; j < 1005; j++)
+					follow_status[i][j] = 0;
+
+			if (run())
+				score = answer_score;
+			else
+				score = 0;
+
+			System.out.println("#" + t + " " + score);
+		}
+	}
 }
